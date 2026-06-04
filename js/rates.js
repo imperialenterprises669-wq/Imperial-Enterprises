119ef0da296a34f58d10160ade4b930f46048ae3
imperialenterprises669-wq <imperialenterprises669@gmail.com>
2026-06-04 21:32:07 +0530

    Update rates.js

--- js/rates.js
+++ js/rates.js
@@ -1,10 +1,10 @@
 const rates = {
-gold999: 154480,
-gold995: 153720,
-goldCoin: 155245,
-silver999: 260347,
-goldSpot: 4445.85,
-silverSpot: 74.09,
-usdInr: 95.72,
+gold999: 156226,
+gold995: 155438,
+goldCoin: 156985,
+silver999: 261305,
+goldSpot: 4477.60,
+silverSpot: 73.90,
+usdInr: 95.77,
 updated: "03 June 2026 9:10 AM"
 };
